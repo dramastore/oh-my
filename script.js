81a1c5f276b1daf318a0719b0825cb98050d806d
dramastore <georgehmariki@gmail.com>
2020-11-09 12:23:22 +0300

    oh my tg

--- script.js
+++ script.js
@@ -1,48 +1,11 @@
-function check() {
-    var input0 = document.getElementById('input0').value;
-    var input1 = document.getElementById('input0');
-    var updater = document.getElementById('ujumbe')
-    var full = document.getElementById('full')
-    var button = document.getElementById('check-code')
-    var clone = document.getElementById('clone')
-    var tryAnother = document.getElementById('try')
-
-    if (input0 == 111) {
-        updater.innerHTML = 'Brazzers Premium Video 111.mp4'
-        updater.style.color = 'blue'
-        full.innerHTML = 'Download Full Video Below 👇'
-        clone.style.display = 'block'
-        clone.setAttribute('onclick', "location.href='https://playtube.ws/9i17kzh8h356.html'")
-        tryAnother.style.display = 'block'
-        button.style.display = 'none'
-        input1.disabled = true
-    }
-
-    if (input0 == 112) {
-        updater.innerHTML = 'Brazzers Premium Video 112.mp4'
-        updater.style.color = 'blue'
-        full.innerHTML = 'Download Full Video Below 👇'
-        clone.style.display = 'block'
-        clone.setAttribute('onclick', "location.href='https://playtube.ws/9i17kzh8h356.html'")
-        tryAnother.style.display = 'block'
-        button.style.display = 'none'
-        input1.disabled = true
-    }
-
-    if (input0 == 113) {
-        updater.innerHTML = 'Brazzers Premium Video 113.mp4'
-        updater.style.color = 'blue'
-        full.innerHTML = 'Download Full Video Below 👇'
-        clone.style.display = 'block'
-        clone.setAttribute('onclick', "location.href='https://playtube.ws/9i17kzh8h356.html'")
-        tryAnother.style.display = 'block'
-        button.style.display = 'none'
-        input1.disabled = true
-    }
-
-    else {
-        document.getElementById('wrong').style.display = 'block'
-    }
-}
+var tarehe = new Date();
+var mwaka = tarehe.getFullYear()
+var mwezi = tarehe.getMonth() + 1
+var leo = tarehe.getDate()
+var saa = tarehe.getHours()
+var dakika = tarehe.getMinutes()
+var sekunde = tarehe.getSeconds()
 
 
+document.getElementById('yaLeo').innerHTML = leo + "/"+ mwezi + "/" + mwaka
+document.getElementById('yaLeo2').innerHTML = leo + "/"+ mwezi + "/" + mwaka
